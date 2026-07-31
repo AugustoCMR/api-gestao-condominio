2445fcc68dad446b9205ea9795540f6489003def
Augusto Ribeiro <augustoribeiro100.g.j@gmail.com>
2026-07-30 21:29:51 -0300

    feat: configura teste de integração e atualiza teste de criação de condominio

--- tests/Feature/Condominium/CreateCondominiumTest.php
+++ tests/Feature/Condominium/CreateCondominiumTest.php
@@ -6,22 +6,22 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class CondominiumTest extends TestCase
-{
-    /**
-     * A basic feature test example.
-     */
+class CreateCondominiumTest extends TestCase
+{   
+    use RefreshDatabase;
     public function test_create_condominium(): void
     {
-        $response = $this->postJson('/api/condominiums', [
+        $response = $this->postJson('/api/condominium', [
             'name' => 'Condomínio Teste',
             'address' => 'Rua Teste, 123',
             'city' => 'Cidade Teste',
             'state' => 'Estado Teste',
             'zip_code' => '12345-678',
-            'cnpj' => '12.345.678/0001-90',
+            'cnpj' => '12345678911111',
             'phone' => '(11) 1234-5678',
-            'email' => 'contato@condominioteste.com.br'
+            'email' => 'contato@condominioteste.com.br',
+            'neighborhood' => 'Teste',
+            'complement' => 'Apto 101'
         ]);
 
         $response->assertCreated();
