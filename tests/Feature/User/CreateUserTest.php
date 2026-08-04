e6f1803de1a72f6c010a1bf2c373deccca00ac4c
Augusto Ribeiro <augustoribeiro100.g.j@gmail.com>
2026-08-03 21:42:54 -0300

    feat: Criando test feature create user

---
+++ tests/Feature/User/CreateUserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CreateUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_create_user(): void
+    {
+        $response = $this->postJson('/api/user/register', [
+            'name' => 'John Doe',
+            'email' => 'john.doe@example.com',
+            'password' => 'password'
+        ]);
+
+        $response->assertCreated();
+    }
+}
