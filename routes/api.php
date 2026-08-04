ec4af239ba841345c4319768f1f0e9794ca7cd25
Augusto Ribeiro <augustoribeiro100.g.j@gmail.com>
2026-08-03 21:43:10 -0300

    feat: Implemetando e ajustando rotas

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,24 @@
 <?php
 
 use App\Http\Controllers\CondominiumController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('user')->name('user.')->group(function ()
+{
+    Route::post('/register', [UserController::class, 'store']);
+});
 
-Route::post('/condominium', [CondominiumController::class, 'store']);
+
+Route::prefix('condominium')->name('condominium')->group(function ()
+{
+    Route::middleware('auth:sanctum')->group(function ()
+    {
+        Route::get('/', [CondominiumController::class, 'index']);
+        Route::post('/condominium', [CondominiumController::class, 'store']);
+        Route::get('/{id}', [CondominiumController::class, 'show']);
+        Route::put('/{id}', [CondominiumController::class, 'update']);
+        Route::delete('/{id}', [CondominiumController::class, 'destroy']);
+    });
+});
